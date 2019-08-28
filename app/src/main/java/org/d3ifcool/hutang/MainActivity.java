28ed5f4c539e0f0c729c78553c60b53a0d662c34
aldi saragih <aldiwahyu.saragih@gmail.com>
2019-08-28 17:31:30 +0700

    Penambahan View Pager (dipinjamkan, meminjam)

--- app/src/main/java/org/d3ifcool/hutang/MainActivity.java
+++ app/src/main/java/org/d3ifcool/hutang/MainActivity.java
@@ -1,5 +1,7 @@
 package org.d3ifcool.hutang;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // untuk pemanggilan slider nya atau view pager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager_Hutang);
+        KategoriHutangAdapter hutangAdapter = new KategoriHutangAdapter(MainActivity.this, getSupportFragmentManager());
+        viewPager.setAdapter(hutangAdapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs_hutang);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
